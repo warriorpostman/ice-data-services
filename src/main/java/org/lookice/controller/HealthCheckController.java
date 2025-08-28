7b57dd3dbf84a065cf7aaa44efa1081684a70b3d
Rustam Kasad <rustamkasad@gmail.com>
2025-08-28 16:30:26 -0700

    add / root healthcheck controller because im stupid at aws

---
+++ src/main/java/org/lookice/controller/HealthCheckController.java
@@ -0,0 +1,13 @@
+package org.lookice.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+    @GetMapping("/")
+    public ResponseEntity<String> root() {
+        return ResponseEntity.ok("OK");
+    }
+}
